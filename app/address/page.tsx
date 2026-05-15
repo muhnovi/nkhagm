00ee1fe2878d325318c7cf7f9da8b7c81ac505a6
muhnovi <muhnoviyanto815@gmail.com>
2026-05-15 15:10:11 +0700

    fix alamat

--- app/address/page.tsx
+++ app/address/page.tsx
@@ -41,7 +41,7 @@ export default function AddressPage() {
           <section className={styles.section}>
             <h2 className={styles.sectionTitle}>Peta Lokasi</h2>
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.521260322283!2d106.8195613!3d-6.2087634!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f3e945e34b9d%3A0x5371bf0fdad786a2!2sJalan%20Jend.%20Sudirman%2C%20Jakarta!5e0!3m2!1sid!2sid!4v1"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3952.9415539223714!2d110.84759047400816!3d-7.796013092224061!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7a31a36e8f73ff%3A0x2fd9d189bf74efbc!2sPondok%20Pesantren%20ISMAKA!5e0!3m2!1sid!2sid!4v1778832462859!5m2!1sid!2sid"
               className={styles.mapFrame}
               loading="lazy"
               title="Lokasi NIIKAHAGAMA"
